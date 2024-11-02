e704ec488501e4ee438d488408611ce3a868084b
Irving <neriairving95@gmail.com>
2024-11-01 19:08:08 -0600

    creando pagina formularios con previsualización de producto

---
+++ carpeta_formularioProductos/fP.js
@@ -0,0 +1,38 @@
+
+    // Aquí se manda a llamar los elementos a la previsualizacion usando los ids de los elementos 
+    
+    document.getElementById('productImage').addEventListener('change', () => {
+        updatePreview();
+      });
+  
+      document.getElementById('productForm').addEventListener('input', () => {
+        updatePreview();
+      });
+  
+      function updatePreview() {
+        const name = document.getElementById('productName').value;
+        const price = document.getElementById('productPrice').value;
+        const description = document.getElementById('productDescription').value;
+        const image = document.getElementById('productImage').files[0];
+
+  
+        if (name || price || description || image) {
+          document.getElementById('previewCard').style.display = 'block';
+          document.getElementById('previewName').innerText = name;
+          document.getElementById('previewPrice').innerText = price;
+          document.getElementById('previewDescription').innerText = description;
+  
+          if (image) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+              document.getElementById('previewImage').src = e.target.result;
+            };
+            reader.readAsDataURL(image);
+          }
+        } else {
+          document.getElementById('previewCard').style.display = 'none';
+        }
+      }
+  
+      // Función para añadir producto a la lista
+      
