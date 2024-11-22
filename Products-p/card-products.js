2a3af55c4d0af987853bd00767f9713217579f5b
Martha Huerta <marthah.112225@gmail.com>
2024-11-22 17:21:14 -0600

    changes in price

--- Products-p/card-products.js
+++ Products-p/card-products.js
@@ -34,7 +34,7 @@ function crearTarjetasArticulosInicio(productos) {
 		nuevoArticulo.innerHTML = `
         <img src=${producto.img}>
         <h3>${producto.name}</h3>
-        <h5>$${producto.precio}</h5>
+        <h5>$${producto.price}</h5>
         <p>${producto.description}</p>
         <button id = "button-tarjeta">Agregar al carrito</button>
         `;
