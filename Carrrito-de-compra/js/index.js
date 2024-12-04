ca2ef92e994f3bdcb23c443757d47728001effa8
Irving <neriairving95@gmail.com>
2024-12-03 19:44:06 -0600

    filtro funcional por precios en carrito de compras

--- Carrrito-de-compra/js/index.js
+++ Carrrito-de-compra/js/index.js
@@ -1,5 +1,10 @@
 const contenedorTarjetas = document.getElementById("productos-container");
 const myInput = document.getElementById("myinput");
+const boxPrice = document.querySelectorAll("ul li button input");
+const priceRange1 = document.getElementById("price-range-1");
+const priceRange2 = document.getElementById("price-range-2");
+const priceRange3 = document.getElementById("price-range-3");
+const priceRange4 = document.getElementById("price-range-4");
 
 function crearTarjetasArticulosInicio(productos) {
   // Limpiar el contenedor antes de agregar nuevas tarjetas
@@ -40,6 +45,63 @@ function initializeProductFilter() {
   });
 }
 
-// Inicializa la página cargando todos los artículos
+// Función para filtrar productos por rango de precios
+function aplicarFiltroPorPrecio() {
+  let filteredPrices = [];
+
+  if (priceRange1.checked) {
+    filteredPrices.push(
+      ...articulos.filter(
+        (articulo) => articulo.precio >= 50 && articulo.precio <= 100
+      )
+    );
+  }
+  if (priceRange2.checked) {
+    filteredPrices.push(
+      ...articulos.filter(
+        (articulo) => articulo.precio >= 101 && articulo.precio <= 250
+      )
+    );
+  }
+  if (priceRange3.checked) {
+    filteredPrices.push(
+      ...articulos.filter(
+        (articulo) => articulo.precio >= 251 && articulo.precio <= 500
+      )
+    );
+  }
+  if (priceRange4.checked) {
+    filteredPrices.push(
+      ...articulos.filter((articulo) => articulo.precio > 500)
+    );
+  }
+
+  if (
+    !priceRange1.checked &&
+    !priceRange2.checked &&
+    !priceRange3.checked &&
+    !priceRange4.checked
+  ) {
+    filteredPrices = articulos;
+  }
+
+  crearTarjetasArticulosInicio(filteredPrices);
+}
+
+boxPrice.forEach((box) => {
+  box.addEventListener("change", aplicarFiltroPorPrecio);
+});
+
+// Event listener para el botón "Limpiar"
+const buttonLimpiar = document.getElementById("button0");
+buttonLimpiar.addEventListener("click", () => {
+  priceRange1.checked = false;
+  priceRange2.checked = false;
+  priceRange3.checked = false;
+  priceRange4.checked = false;
+
+  crearTarjetasArticulosInicio(articulos);
+});
+
 crearTarjetasArticulosInicio(articulos);
 initializeProductFilter();
